088c24754bfa889ffbf074a9571530950bbf120b
David Zonana <Davidzn2@users.noreply.github.com>
2023-11-01 17:43:46 -0600

    Logo bonds En projetcs

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import Header from '../../Components/Layout/Header'
 import Footer from '../../Components/Layout/Footer'
 import AirtableDataFetcher from '../../Components/dataFetcher';
+import ibonds from '../../assets/LOGOS/israel-bonds-logo.jpg'
 
 function Projects() {
   return (
@@ -8,6 +9,9 @@ function Projects() {
       <Header />
       <div className="container  mx-auto">
         <h1 className="text-3xl font-bold text-center	m-10">Vota por tu proyecto favorito</h1>
+        <div className="flex items-center justify-center ">
+          <img src={ibonds} className="w-1/5" alt="" />
+        </div>
         <AirtableDataFetcher />
       </div>
       <Footer />
