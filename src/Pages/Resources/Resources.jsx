7a70831237386b57d97af53a607132d7015fe22f
David Zonana <Davidzn2@users.noreply.github.com>
2023-11-26 20:41:27 -0600

    Fix fix

--- src/Pages/Resources/Resources.jsx
+++ src/Pages/Resources/Resources.jsx
@@ -19,7 +19,7 @@ import concept from '../../assets/concept.png'
 import entrevistas from '../../assets/entrevista.png'
 import mentor from '../../assets/mentor.png'
 import productCanvas from '../../assets/product-canvas.png'
-import calendarNew from '../../assets/calendarioNew.png'
+import calendarNew from '../../assets/calendarionew.png'
 function Recursos() {
   return (
     <>
